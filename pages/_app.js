14192f8b39550c567649a51b77d02f9102a82e62
Rtuber1231 <rtuber1231@gmail.com>
2025-08-22 23:47:56 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,15 @@
+// FILE: /pages/_app.js
 import '../styles/globals.css';
 import Layout from '../components/Layout';
+import { SessionProvider } from '../context/SessionContext';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <SessionProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </SessionProvider>
   );
 }
 
